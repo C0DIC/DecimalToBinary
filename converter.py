e93a06c52eaea95272abf0a1ba1a5a3c5353350b
C0DIC <michaellivesforever23@gmail.com>
2022-04-12 02:22:53 +0400

    Small changes

--- converter.py
+++ converter.py
@@ -32,7 +32,7 @@ class Converter:
             print(f"{self.input_data} in decimal system is {self.output_data}")
 
 
-    def converter(self, input_data) -> int:
+    def converter(self, input_data):
         self.input_data = input_data
 
         try:
@@ -41,20 +41,22 @@ class Converter:
             output_data = []
 
             while (new_input_data / 2)!= 0:
-                output_data.append(new_input_data % 2)
+                output_data.append(str(new_input_data % 2))
                 new_input_data = new_input_data // 2
 
             output_data = output_data[::-1]
             new_output_data = ""
 
+            
             for x in output_data:
-                new_output_data += str(x)
+                new_output_data += x
 
             return new_output_data 
         except (TypeError, ValueError):
             print("Input data can not be converted to integer, please try again")
             time.sleep(2)
             self.main()
+            
 
 cv = Converter()
 cv.main()
